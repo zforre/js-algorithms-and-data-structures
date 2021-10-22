86cc85e2d0e47174120202912bf1d41da2f170f3
Zach Forrest <59568729+zforre@users.noreply.github.com>
2021-10-21 23:17:30 -0400

    create problem solving note

---
+++ problemSolving.js
@@ -0,0 +1,35 @@
+// An algorithm is a process or set of steps to accomplish a certain task.
+
+// George Polya - How To Solve It
+
+// Understand the problem
+//  Can I restate the problem in my own words
+//  What are the inputs that go into it
+//  What outputs should come from the solution
+//  Can the ouputs be determined from the inputs
+//  How should I label the important pieces of data
+
+// Explore concrete examples
+//  Start with simple examples
+//  Progress to more complex examples
+//  Explore examples with empty inputs
+//  Explore examples with invalid inputs
+
+// Break it down
+//  Explicitly write out the steps you need to take
+
+// Solve/Simplify
+//  Solve the problem if you can, solve a simpler version of the problem if you cant
+//  Find the difficulty
+//  Temporarily ignore it
+//  Write a simplified solution
+//  Now incorporate the difficulty
+
+// Look back and refactor
+//  Can you check the result
+//  Can you derive the result differently
+//  Can you understand it at a glance
+//  Can you use the result or method for other problems
+//  Can you improve the performance
+//  Can you think of other ways to refactor
+//  How have other people solved this problem
